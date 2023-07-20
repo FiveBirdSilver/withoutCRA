8a9be116879ee1fa2b243e660b13000bae8c97b7
seo <seo@goldenplanet.co.kr>
2023-07-20 13:58:01 +0900

    MUI 라이브러리 적용

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,38 @@
 import React, { useEffect, useState } from "react";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 import { getData, setData } from "./apis";
 
 function App() {
   const [title, setTitle] = useState("");
   const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("Male");
 
   const getPosts = async () => {
-    await getData();
+    await getData().then((res) => console.log(res));
   };
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = event.target;
-    if (id === "title") setTitle(value);
+    const { name, value } = event.target;
+    if (name === "title") setTitle(value);
     else setEmail(value);
+  };
+
+  const handleOnCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    if (value === "Male" && checked) setGender("Male");
+    else if (value === "Female" && checked) setGender("Female");
+    else return;
   };
 
   const handleOnCreate = async () => {
     const sendData = {
       title: title,
       email: email,
-      gender: "male",
+      gender: gender,
     };
     await setData(sendData);
   };
@@ -31,9 +43,36 @@ function App() {
 
   return (
     <div>
-      <input id="title" value={title} onChange={handleOnChange} placeholder="이름" />
-      <input id="email" value={email} onChange={handleOnChange} placeholder="이메일" />
-      <button onClick={handleOnCreate}>리스트 추가</button>
+      <div style={{ display: "flex", alignContent: "center", justifyContent: "center", gap: "20px" }}>
+        <TextField
+          id="outlined-basic"
+          label="이름"
+          name="title"
+          variant="outlined"
+          value={title}
+          onChange={handleOnChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="이메일"
+          name="email"
+          variant="outlined"
+          value={email}
+          onChange={handleOnChange}
+        />
+        <FormControlLabel
+          control={<Checkbox onChange={handleOnCheck} value="Male" checked={gender === "Male"} />}
+          label="남"
+        />
+        <FormControlLabel
+          control={<Checkbox onChange={handleOnCheck} value="Female" checked={gender === "Female"} />}
+          label="여"
+        />
+        <Button variant="outlined" onClick={handleOnCreate}>
+          추가
+        </Button>
+      </div>
+      <div></div>
     </div>
   );
 }
